jeffmerkey/mutter

commit 6bee1e5b5855357b69cde296caec0fe877a7712c

window: Introduce 'target monitor' concept
This, in contrast to the 'main monitor', represents the monitor window
management routines should target when applied, while the 'main monitor'
represents the monitor the window is mostly on given its current visible
state.

They will differ primarily when doing window management operations that
explicitly move a window to another monitor, at which point window
operations should not act on the monitor it is currently visible on, but
the monitor that it's intended to be moved to.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/4960>

src/core/window.c
@@ -385,6 +385,7 @@ meta_window_finalize (GObject *object)
   g_clear_pointer (&window->preferred_logical_monitor,
                    meta_logical_monitor_id_free);
   g_clear_object (&window->monitor);
+  g_clear_object (&window->target_monitor);
   g_clear_object (&window->highest_scale_monitor);
   g_clear_object (&window->config);
 
@@ -1255,6 +1256,7 @@ meta_window_constructed (GObject *object)
       highest_scale_monitor = main_monitor;
     }
   g_set_object (&window->monitor, main_monitor);
+  g_set_object (&window->target_monitor, main_monitor);
   g_set_object (&window->highest_scale_monitor, highest_scale_monitor);
 
   if (window->monitor)
@@ -3305,7 +3307,14 @@ meta_window_tile_internal (MetaWindow   *window,
   else if (meta_window_config_get_tile_monitor_number (window->config) < 0)
     {
       meta_window_config_set_tile_monitor_number (window->config,
-                                                  window->monitor->number);
+                                                  window->target_monitor->number);
+    }
+  else
+    {
+      int number;
+
+      number = meta_window_config_get_tile_monitor_number (window->config);
+      meta_window_set_target_monitor_from_number (window, number);
     }
 
   if (tile_mode == META_TILE_MAXIMIZED)
@@ -4068,7 +4077,7 @@ meta_window_update_for_monitors_changed (MetaWindow *window)
   MetaMonitorManager *monitor_manager =
     meta_backend_get_monitor_manager (backend);
 #endif
-  const MetaLogicalMonitor *old, *new;
+  MetaLogicalMonitor *old, *new;
 
   if (meta_window_has_fullscreen_monitors (window))
     meta_window_clear_fullscreen_monitors (window);
@@ -4095,6 +4104,8 @@ meta_window_update_for_monitors_changed (MetaWindow *window)
       meta_window_config_set_tile_monitor_number (window->config,
                                                   new_monitor_number);
     }
+
+  g_set_object (&window->target_monitor, new);
 
   if (new && old)
     {
@@ -4197,6 +4208,9 @@ meta_window_move_resize_internal (MetaWindow          *window,
    * to the client.
    */
 
+  MetaBackend *backend = backend_from_window (window);
+  MetaMonitorManager *monitor_manager =
+    meta_backend_get_monitor_manager (backend);
   MetaWorkspaceManager *workspace_manager = window->display->workspace_manager;
   gboolean did_placement;
   MtkRectangle unconstrained_rect;
@@ -4207,6 +4221,7 @@ meta_window_move_resize_internal (MetaWindow          *window,
   int rel_y = 0;
   MetaMoveResizeResultFlags result = 0;
   gboolean moved_or_resized = FALSE;
+  MetaLogicalMonitor *old_monitor;
   MetaWindowUpdateMonitorFlags update_monitor_flags;
   MetaGravity gravity;
 
@@ -4306,9 +4321,6 @@ meta_window_move_resize_internal (MetaWindow          *window,
            !mtk_rectangle_is_empty (&unconstrained_rect) &&
            !meta_window_config_is_floating (window->config))
     {
-      MetaBackend *backend = backend_from_window (window);
-      MetaMonitorManager *monitor_manager =
-        meta_backend_get_monitor_manager (backend);
       MetaLogicalMonitor *from;
       MetaLogicalMonitor *to;
 
@@ -4400,6 +4412,7 @@ meta_window_move_resize_internal (MetaWindow          *window,
   if (flags & META_MOVE_RESIZE_FORCE_UPDATE_MONITOR)
     update_monitor_flags |= META_WINDOW_UPDATE_MONITOR_FLAGS_FORCE;
 
+  old_monitor = window->monitor;
   if (window->monitor)
     {
       g_autoptr (MetaLogicalMonitorId) old_id = NULL;
@@ -4422,6 +4435,9 @@ meta_window_move_resize_internal (MetaWindow          *window,
     {
       meta_window_update_monitor (window, update_monitor_flags);
     }
+
+  if (old_monitor != window->monitor)
+    meta_window_set_target_monitor (window, window->monitor);
 
   meta_window_foreach_transient (window, maybe_move_attached_window, NULL);
 
@@ -4582,6 +4598,8 @@ meta_window_move_to_monitor_internal (MetaWindow          *window,
                                       int                  monitor)
 {
   MtkRectangle old_area, new_area;
+
+  meta_window_set_target_monitor_from_number (window, monitor);
 
   if (meta_window_config_get_tile_mode (window->config) != META_TILE_NONE)
     meta_window_config_set_tile_monitor_number (window->config, monitor);
@@ -8934,4 +8952,27 @@ meta_window_get_max_size (MetaWindow *window,
 
       return FALSE;
     }
+}
+
+void
+meta_window_set_target_monitor (MetaWindow         *window,
+                                MetaLogicalMonitor *logical_monitor)
+{
+  g_return_if_fail (logical_monitor);
+  g_set_object (&window->target_monitor, logical_monitor);
+}
+
+void
+meta_window_set_target_monitor_from_number (MetaWindow *window,
+                                            int         number)
+{
+  MetaBackend *backend = backend_from_window (window);
+  MetaMonitorManager *monitor_manager =
+    meta_backend_get_monitor_manager (backend);
+  MetaLogicalMonitor *logical_monitor;
+
+  logical_monitor =
+    meta_monitor_manager_get_logical_monitor_from_number (monitor_manager,
+                                                          number);
+  meta_window_set_target_monitor (window, logical_monitor);
 }

src/core/constraints.c
@@ -469,8 +469,9 @@ setup_constraint_info (MetaBackend         *backend,
 
       if (!logical_monitor)
         {
-          meta_topic (META_DEBUG_GEOMETRY, "Constraining using window monitor");
-          logical_monitor = window->monitor;
+          meta_topic (META_DEBUG_GEOMETRY,
+                      "Constraining using window target monitor");
+          logical_monitor = window->target_monitor;
         }
     }
 

src/core/window-private.h
@@ -240,6 +240,7 @@ struct _MetaWindow
   uint64_t id;
   guint64 stamp;
   MetaLogicalMonitor *monitor;
+  MetaLogicalMonitor *target_monitor;
   MetaLogicalMonitor *highest_scale_monitor;
   MetaWorkspace *workspace;
   MetaWindowClientType client_type;
@@ -952,3 +953,9 @@ void meta_window_set_a11y_properties (MetaWindow *window,
 gboolean meta_window_get_a11y_properties (MetaWindow  *window,
                                           const char **a11y_dbus_name,
                                           const char **toplevel_object_path);
+
+void meta_window_set_target_monitor (MetaWindow         *window,
+                                     MetaLogicalMonitor *logical_monitor);
+
+void meta_window_set_target_monitor_from_number (MetaWindow *window,
+                                                 int         number);

commit 371973f3d05db3f7663dada09a7197ed3f1142af

core/window: Refresh target monitor on monitor changes
Windows of type DESKTOP and override_redirect skip the normal monitor
refresh path during monitor reconfiguration, which can leave target_monitor
stale after hotplug or mode changes.

Re-sync the desktop window target monitor from the updated main monitor
so later constraint code does not follow a stale logical monitor reference.

Fixes: 6bee1e5b58 ("window: Introduce 'target monitor' concept")

Closes: https://gitlab.gnome.org/GNOME/mutter/-/work_items/4891
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5151>

src/core/window.c
@@ -4096,6 +4096,7 @@ meta_window_update_for_monitors_changed (MetaWindow *window)
     {
       meta_window_update_monitor (window,
                                   META_WINDOW_UPDATE_MONITOR_FLAGS_FORCE);
+      g_set_object (&window->target_monitor, window->monitor);
       goto out;
     }
 