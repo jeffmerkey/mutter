jeffmerkey/mutter

commit 14b3d57b7d92a9cc36e495f2a914548cd9b2ca9b

kms-impl-device: Discard pending update when discarding pending flips
If we:

  1) Draw a frame, and queue to KMS thread
  2) Process an update that expects a page flip
  3) Draw another frame, but postpone awaiting page flip callback
  4) Page flip callback reaches MetaOnscreeNative
  5) Postponed frame queued to KMS thread
  6) Renderer rebuilds view and discards pending page flips
  7) As part of the sync call, the frame from step 3 is now processed,
     but queued, due to the page flip callback hasn't reached
     MetaKmsImplDevice yet
  8) Receive the page flip callback from step 1

We'd hit a warning about an unexpected pending update. Discarding it
when discarding pending page flips avoids this also for updates that had
not yet been processed, and doig so is safe, since we only ever discard
pending updates when we're about to do a full reset of the complete
state.

Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5063>
(cherry picked from commit 985bece7da9e321bf9c83d27e7a36d8b5dc730ba)

src/backends/native/meta-kms-impl-device.c
@@ -2454,6 +2454,14 @@ void
 meta_kms_impl_device_discard_pending_page_flips (MetaKmsImplDevice *impl_device)
 {
   MetaKmsImplDeviceClass *klass = META_KMS_IMPL_DEVICE_GET_CLASS (impl_device);
+  MetaKmsImplDevicePrivate *priv =
+    meta_kms_impl_device_get_instance_private (impl_device);
+  GHashTableIter iter;
+  CrtcFrame *crtc_frame;
+
+  g_hash_table_iter_init (&iter, priv->crtc_frames);
+  while (g_hash_table_iter_next (&iter, NULL, (gpointer *) &crtc_frame))
+    discard_update (impl_device, &crtc_frame->pending_update);
 
   klass->discard_pending_page_flips (impl_device);
 }

commit 5d187100e2e146511cc19269d2f1c650cc3125ab

kms/impl-device: Handle NULL crtc_frames in _discard_pending_page_flips
Closes: https://gitlab.gnome.org/GNOME/mutter/-/work_items/4848
Fixes: 14b3d57b7d92 ("kms-impl-device: Discard pending update when discarding pending flips")
Part-of: <https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/5163>

src/backends/native/meta-kms-impl-device.c
@@ -2457,12 +2457,16 @@ meta_kms_impl_device_discard_pending_page_flips (MetaKmsImplDevice *impl_device)
   MetaKmsImplDeviceClass *klass = META_KMS_IMPL_DEVICE_GET_CLASS (impl_device);
   MetaKmsImplDevicePrivate *priv =
     meta_kms_impl_device_get_instance_private (impl_device);
-  GHashTableIter iter;
-  CrtcFrame *crtc_frame;
 
-  g_hash_table_iter_init (&iter, priv->crtc_frames);
-  while (g_hash_table_iter_next (&iter, NULL, (gpointer *) &crtc_frame))
-    discard_update (impl_device, &crtc_frame->pending_update);
+  if (priv->crtc_frames)
+    {
+      GHashTableIter iter;
+      CrtcFrame *crtc_frame;
+
+      g_hash_table_iter_init (&iter, priv->crtc_frames);
+      while (g_hash_table_iter_next (&iter, NULL, (gpointer *) &crtc_frame))
+        discard_update (impl_device, &crtc_frame->pending_update);
+    }
 
   klass->discard_pending_page_flips (impl_device);
 }